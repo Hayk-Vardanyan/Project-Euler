c1f11b00b3105db51a779f2f2a7afb818abae39a
HaykVardanyan2004 <90803123+HaykVardanyan2004@users.noreply.github.com>
2022-02-15 00:00:41 +0400

    Update problem15.cpp

--- problem15.cpp
+++ problem15.cpp
@@ -4,22 +4,39 @@
 std::string factorial(int number)
 {
     std::string sum = "1";
-   
-    for(int i = 1; i <= number; ++i)
+    
+    for(int i = number; i > 1; --i)
     {
-        sum = std::to_string(i * stod(sum));
+        sum = std::to_string(i * (std::stod(sum)));
     }
-   
+    
     return sum;
 }
 
 
+std::string count_paths(int size)
+{
+    int n = size*2;
+    int k = size;
+    
+    double d = std::stod(factorial(n)) / (std::stod(factorial(n - k)) * std::stod(factorial(k)));
+    std::string res = std::to_string(d);
+  
+    std::string tmp{};
+   
+   for(int i = 0; i < res.size(); ++i)
+   {
+       if(res[i] == '.') break;
+       
+       tmp.push_back(res[i]);
+   }
+ 
+    res = tmp;
+    return res;
+    
+}
+
 int main()
 {
-    int n = 40;
-    int k = 20;
-
-
-    double d = std::stod(factorial(n)) / (std::stod(factorial(n - k)) * std::stod(factorial(k)));
-    std::cout << std::to_string(d);
+    std::cout << count_paths(7);
 }
